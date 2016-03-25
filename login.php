9419610ca0f31ee48322ee233dc1549e7a4af5f7
Nemanja Djokic <nemanja.djokic@gmail.com>
2016-03-25 15:13:19 +0100

    Login page added

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login page</title>
+	<link rel="stylesheet" href="foundation-6/css/foundation.css" />
+</head>
+<body>
+	<div class="row">
+		<div class="small-6 large-expand columns"><h1>This is the service login page</h1></div>
+	</div>
+</body>
+</html>
